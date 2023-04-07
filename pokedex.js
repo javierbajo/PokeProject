2362b12f401e7904aecda2b964a8fcbac6564405
JaviBajo <javierbajobarrio@gmail.com>
2023-04-08 00:28:44 +0200

    Añadido input selector de pokemons

--- pokedex.js
+++ pokedex.js
@@ -25,7 +25,8 @@ const mapearPokemons = (pokemon) => {
 }
 
 const draw = (pokemonMapeados) => {
-
+    
+    pokedex$$.innerHTML = "";
     for(const pokemon of pokemonMapeados){
         const li$$ = document.createElement('li');
         li$$.innerHTML = `
@@ -36,19 +37,36 @@ const draw = (pokemonMapeados) => {
     }
 }
 
+const buscarPokemons = (filtro, pokemonMapeados) => {
+    let pokemonFiltrados = pokemonMapeados.filter((pokemon) => pokemon.nombre.toLowerCase().includes(filtro.toLowerCase()));
+    // le mando los pokemos filtrados a la función que pinta
+    console.clear();
+    console.log(pokemonFiltrados);
+    draw(pokemonFiltrados);
+};
+
+const takeInput = (pokemonMapeados) => {
+    const input$$ = document.querySelector('input');
+    // creo un evento input y mando el contenido del input y el array de pokemons mapeados a la función buscarPokemons
+    input$$.addEventListener('input', () => buscarPokemons(input$$.value, pokemonMapeados));
+}
+
 const init = async () => {
 
+    // llamar a la función get 150 veces, una para cada URL
     for(let i = 1; i <= numPokemons; i++){
         pokemons.push(await get(i));
     }
     // hasta ahora tenemos en pokemons un array, en el que cada elemento tiene toda la información de un individuo
-    console.log(pokemons.length)
-    console.log(pokemons)
+    console.log(pokemons.length);
+    console.log(pokemons);
     // ahora lo vamos a mapear para quedarme con el nombre y la imagen
     const pokemonMapeados = mapearPokemons(pokemons);
     console.log(pokemonMapeados);
     // ahora a ver si los pintamos
-    draw(pokemonMapeados)
+    draw(pokemonMapeados);
+    // creamos un index en el html para filtrar pokemons
+    takeInput(pokemonMapeados);
 
 }
 
